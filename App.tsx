eeb8b0157b8f888390ca1fa7351091fa9a038a2f
anavilyadav <anavil.yadav92@gmail.com>
2026-07-21 11:53:48 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { supabase } from './lib/supabase'
+import { supabase } from './src/lib/supabase'
 import ReceptionApp from './reception'
 import DoctorApp from './doctor'
 import PharmacyApp from './pharmacy'
